607417c528b9fac1e4e59f7c6f511f64a8b4b0c2
wenooij <142677213+wenooij@users.noreply.github.com>
2024-01-12 20:32:14 -0500

    Backprop: Add basic test

---
+++ backprop_test.go
@@ -0,0 +1,58 @@
+package mcts
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestBackprop(t *testing.T) {
+	r := rand.New(rand.NewSource(1337))
+	s := &Search[dummyStep]{SearchInterface: dummySearch{Rand: r}, Rand: r}
+	s.root = newTree(s)
+	leaf := s.root
+	for i := 0; i < 3; i++ {
+		leaf, _ = getOrCreateChild(s, leaf, FrontierStep[dummyStep]{})
+	}
+	log, numRollouts := s.Rollout()
+	backprop(leaf, log, numRollouts)
+
+	const score = 0.6287385421322026
+	got := s.PV()
+	want := Variation[dummyStep]{{
+		Step:        dummyStep(0),
+		Log:         dummyScalarLog(score),
+		Score:       score,
+		RawScore:    score,
+		NumRollouts: 1,
+		Priority:    -score,
+		NumChildren: 1,
+	}, {
+		Step:        dummyStep(0),
+		Log:         dummyScalarLog(score),
+		Score:       score,
+		RawScore:    score,
+		NumRollouts: 1,
+		Priority:    -score,
+		NumChildren: 1,
+	}, {
+		Step:        dummyStep(0),
+		Log:         dummyScalarLog(score),
+		Score:       score,
+		RawScore:    score,
+		NumRollouts: 1,
+		Priority:    -score,
+		NumChildren: 1,
+	}, {
+		Step:        dummyStep(0),
+		Log:         dummyScalarLog(score),
+		Score:       score,
+		RawScore:    score,
+		NumRollouts: 1,
+		Priority:    -score,
+	}}
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("TestBackprop(): got diff (-want, +got):\n%s", diff)
+	}
+}
